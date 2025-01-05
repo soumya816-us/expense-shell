33ed1e391c6075ba6e28fc67c4845e7485dcf653
soumya816-us <soumyareddy816@gmail.com>
2025-01-04 23:26:25 -0500

    expense shell

---
+++ frontend.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+USERID=$(id -u) # checking is it root user or not
+
+#colors added to the script
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/expense-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+
+VALIDATE(){
+
+ if [ $1 -ne 0 ] # again checking installation is success or not
+    then 
+        echo -e " $2 .... $R Failure $N "
+        exit 1
+    else
+        echo -e " $2 .... $G Success $N "
+    fi
+
+}
+
+
+CHECK_ROOT(){
+if [ $USERID -ne 0 ] 
+then 
+    echo "ERROR:: you must have sudo access to execute script"
+    exit 1 # other than exit 0 you can give any number "exit 0 means success"
+fi
+}
+mkdir -p $LOGS_FOLDER
+
+echo "script started executing at :: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT
+
+dnf install nginx -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing Nginx server"
+
+systemctl enable nginx &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling Nginx Server"
+
+systemctl start nginx &>>$LOG_FILE_NAME
+VALIDATE $? "Starting Nginx Server"
+
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE_NAME
+VALIDATE $? "Removing existing version of code"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE_NAME
+VALIDATE $? "Downloading the Latest code"
+
+cd /usr/share/nginx/html  &>>$LOG_FILE_NAME
+VALIDATE $? "Moving to HTML Directory"
+
+unzip /tmp/frontend.zip  &>>$LOG_FILE_NAME
+VALIDATE $? "unzipping the frontend code"
+
+systemctl restart nginx  &>>$LOG_FILE_NAME
+VALIDATE $? "Restarting Nginx Server"
