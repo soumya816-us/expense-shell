ce81b08bb11bc8d11dfe3f21512cc176819e4426
soumya816-us <soumyareddy816@gmail.com>
2025-01-04 23:32:06 -0500

    expense shell

--- frontend.sh
+++ frontend.sh
@@ -61,5 +61,8 @@ VALIDATE $? "Moving to HTML Directory"
 unzip /tmp/frontend.zip  &>>$LOG_FILE_NAME
 VALIDATE $? "unzipping the frontend code"
 
+cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
+VALIDATE $? "coping expense.config"
+
 systemctl restart nginx  &>>$LOG_FILE_NAME
 VALIDATE $? "Restarting Nginx Server"
