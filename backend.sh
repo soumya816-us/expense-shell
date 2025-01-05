56e17716aa0fc58fd35ca875d4a57be5cff60d9f
soumya816-us <soumyareddy816@gmail.com>
2025-01-04 23:07:51 -0500

    expense shell

--- backend.sh
+++ backend.sh
@@ -66,6 +66,7 @@ curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expen
 VALIDATE $? " Downloading Backend"
 
 cd /app
+rm -rf /app/*
 
 unzip /tmp/backend.zip &>>$LOG_FILE_NAME
 VALIDATE $? " Unzip the Downloaded Backend code"
